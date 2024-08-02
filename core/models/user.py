7ae7b930d268c0465b016041c64905aa1c61e350
Kristina Shishkina <klikklok2017@mail.ru>
2024-08-02 17:29:43 +0300

    create reletionships beetwen user and post

--- core/models/user.py
+++ core/models/user.py
@@ -1,9 +1,17 @@
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .post import Post
+
 
 class User(Base):
 
     username: Mapped[str] = mapped_column(String(32), unique=True)
     email: Mapped[str] 
+
+    posts: Mapped[list["Post"]] = relationship(back_populates="user")
