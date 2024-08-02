b4a349360800ab968c205ef4b41298e23896b9a7
Kristina Shishkina <klikklok2017@mail.ru>
2024-08-02 16:48:19 +0300

    add asynccontextmanager

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+from contextlib import asynccontextmanager
 from typing import Annotated
 from fastapi import FastAPI, Query
 
+import uvicorn
+
+from core.config import settings
+from core.models import Base, db_helper
+from app_v1 import router as routers_v1
 from items_views import router as items_router
 from users.views import router as users_router
 
-import uvicorn
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router=routers_v1, prefix=settings.api_v1_prefix)
 app.include_router(items_router)
 app.include_router(users_router)
 
