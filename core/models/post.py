e202cc79a99786248d110df7b819a36a3a7c3862
Kristina Shishkina <klikklok2017@mail.ru>
2024-08-02 18:26:23 +0300

    craete user relation mixin

--- core/models/post.py
+++ core/models/post.py
@@ -1,24 +1,18 @@
-from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 
-from typing import TYPE_CHECKING
+from  .mixins import UserRelationsMixin
 
 from .base import Base
 
-if TYPE_CHECKING:
-    from .user import User 
 
-class Post(Base):
+class Post(UserRelationsMixin, Base):
 
-    title: Mapped[str] = mapped_column(String(100))
+    _user_back_populates = 'posts'
+
+    title: Mapped[str] = mapped_column(String(100), unique=False)
     body: Mapped[str] = mapped_column(
         Text,
         default="", # на стороне алхимии
         server_default="", # на стороне бд
     )
-
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey('users.id'),
-    )
-
-    user: Mapped["User"] = relationship(back_populates="posts")
