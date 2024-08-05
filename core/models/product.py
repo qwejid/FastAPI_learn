8e2e3dcc237cf4fd96cd124120a2cc220ccddfa9
Kristina Shishkina <klikklok2017@mail.ru>
2024-08-05 15:24:18 +0300

    add m2m order product

--- core/models/product.py
+++ core/models/product.py
@@ -1,9 +1,23 @@
-from .base import Base
-from sqlalchemy.orm import Mapped
+from typing import TYPE_CHECKING
 
+from .base import Base
+from sqlalchemy.orm import Mapped, relationship
+
+if TYPE_CHECKING:
+    from .order import Order
+    from .order_product_association import OrderProductAsociationTable
 
 class Product(Base):
 
     name: Mapped[str]
     description: Mapped[str]
     price: Mapped[int]
+
+    # orders: Mapped[list["Order"]] = relationship(
+    #     secondary="order_product_association_table",
+    #     back_populates="products",
+    # )
+
+    orders_detais: Mapped[list["OrderProductAsociationTable"]] = relationship(
+        back_populates='product',
+    )
