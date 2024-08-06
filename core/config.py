df9aa01f96194f694e482ef56f95e4e17cc4b2a2
Kristina Shishkina <klikklok2017@mail.ru>
2024-08-06 15:38:17 +0300

    add AuthJWT

--- core/config.py
+++ core/config.py
@@ -9,9 +9,17 @@ class DbSettings(BaseModel):
     url: str = f"sqlite+aiosqlite:///{DB_PATH}"
     echo: bool = False
 
+class AuthJWT(BaseModel):
+    private_key_path: Path = BASE_DIR / 'certs' / 'jwt-private.pem'
+    public_key_path: Path = BASE_DIR / 'certs' / 'jwt-public.pem'
+    algorithm: str = 'RS256'
+    access_token_exp_minutes: int = 15
+
 class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
     db: DbSettings = DbSettings()
+
+    auth_jwt: AuthJWT = AuthJWT()
 
 
 settings = Settings()
