fca6be32e65aeb7898cdc6330a1dcf94b78740e9
Kristina Shishkina <klikklok2017@mail.ru>
2024-08-06 15:37:39 +0300

    add UserSchema

--- users/schemas.py
+++ users/schemas.py
@@ -1,13 +1,17 @@
 from typing import Annotated
 from fastapi import Body
 from annotated_types import MaxLen, MinLen
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, ConfigDict
 
 
 class CreateUser(BaseModel):
     username: Annotated[str, MinLen(3), MaxLen(20)]
     email: Annotated[EmailStr, Body()]
 
-
-class DeleteUser(BaseModel):
-    pass
+class UserSchema(BaseModel):
+    model_config = ConfigDict(strict=True)
+    
+    username: str
+    password: bytes
+    email: EmailStr | None = None
+    active: bool =True
